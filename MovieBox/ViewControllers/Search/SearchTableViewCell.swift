b55af475696e3ad9131e0a67264d34f30c656962
MartyTheGout <chihyung0@naver.com>
2025-01-28 00:01:52 +0900

    [SearchViewController] Added with Test data on viewDidLoad

--- MovieBox/ViewControllers/Search/SearchTableViewCell.swift
+++ MovieBox/ViewControllers/Search/SearchTableViewCell.swift
@@ -6,18 +6,120 @@
 //
 
 import UIKit
+import SnapKit
+import Kingfisher
 
-class SearchTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+final class SearchTableViewCell: BaseTableViewCell {
+    
+    static var id : String { String (describing: self) }
+    
+    var searcheMovie: TrendingMovie?
+    
+    let mainImage : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    let titleLabel : UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.font = UIFont.systemFont(ofSize: 17, weight: .medium)
+        label.textColor = AppColor.mainInfoDeliver.inUIColorFormat
+        return label
+    }()
+    
+    let dateLabel : UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        label.font = UIFont.systemFont(ofSize: 13, weight: .medium)
+        label.textColor = AppColor.subBackground.inUIColorFormat
+        return label
+    }()
+    
+    let genreStack: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 4
+        return stackView
+    }()
+    
+    let likeButton : UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(AppSFSymbol.whiteHeart.image, for: .normal)
+        button.tintColor = AppColor.tintBlue.inUIColorFormat
+        return button
+    }()
+    
+    let genreLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = AppColor.mainInfoDeliver.inUIColorFormat
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.backgroundColor = AppColor.cardBackground.inUIColorFormat
+        return label
+    }()
+    
+    override func configureViewHierarchy() {
+        [mainImage, titleLabel, dateLabel, genreStack, likeButton].forEach{
+            contentView.addSubview($0)
+        }
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    override func configureViewLayout() {
+        mainImage.snp.makeConstraints {
+            $0.top.leading.equalTo(contentView).offset(16)
+            let width: CGFloat = UIScreen.main.bounds.width / 4
+            $0.width.equalTo(width)
+            $0.height.equalTo(width).multipliedBy(1.2)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(contentView).offset(16)
+            $0.leading.equalTo(mainImage.snp.trailing).offset(16)
+        }
+        
+        dateLabel.snp.makeConstraints{
+            $0.top.equalTo(titleLabel.snp.bottom).offset(16)
+            $0.leading.equalTo(mainImage.snp.trailing).offset(16)
+        }
+        
+        genreStack.snp.makeConstraints {
+            $0.leading.equalTo(mainImage.snp.trailing).offset(16)
+            $0.bottom.equalTo(contentView).offset(-16)
+        }
+        
+        likeButton.snp.makeConstraints {
+            $0.trailing.equalTo(contentView).offset(-16)
+            $0.bottom.equalTo(contentView).offset(-16)
+        }
     }
-
+    
+    override func configureViewDetails() {
+        contentView.backgroundColor = AppColor.mainBackground.inUIColorFormat
+    }
+    
+    func fillUpData(with data: SearchedMovie) {
+        mainImage.kf.setImage(with: URL(string: Datasource.baseImageURL.rawValue + data.posterPath)!)
+        
+        titleLabel.text = data.title
+        dateLabel.text = data.releaseDate
+        
+        data.genreIDS.forEach { (id) in
+            let genreInKorean = Genre(rawValue: id)?.koreanName
+            
+            let label = {
+                let label = UILabel()
+                label.text = genreInKorean
+                label.textColor = AppColor.mainInfoDeliver.inUIColorFormat
+                label.font = UIFont.systemFont(ofSize: 13)
+                label.backgroundColor = AppColor.cardBackground.inUIColorFormat
+                label.layer.cornerRadius = label.frame.height / 2
+                return label
+            }()
+            
+            genreStack.addArrangedSubview(label)
+        }
+    }
+    
 }
--- MovieBox/ViewControllers/Search/SearchViewController.swift
+++ MovieBox/ViewControllers/Search/SearchViewController.swift
@@ -8,32 +8,93 @@ import UIKit
 import SnapKit
 
 final class SearchViewController: BaseViewController {
-
-    var currentKeyoword : String?
     
-    let label: UILabel = {
-        let label = UILabel()
-        label.text = "Search View is under construction"
-        
-        return label
+    var currentKeyoword : String?
+    var page = 1
+    
+    var data: [SearchedMovie] = [] {
+        didSet {
+            print(data)
+            tableView.reloadData()
+        }
+    }
+    
+    let searchBar : UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "찾고 싶은 영화를 검색해주세요."
+        return searchBar
     }()
     
+    let tableView : UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "\(label.text)/ current keyword is \(currentKeyoword)"
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.id)
+        
+        NetworkManager.shared.callRequest(apiKind: .search(query: "소닉", page: page)) { (response: SearchResponse) -> Void in
+            self.data = response.results
+        } failureHandler: { afError, responseError in
+            dump(afError)
+            dump(responseError)
+        }
+    }
+    
+    override func setInitialValue() {
+        navigationName = "영화 검색"
     }
     
     override func configureViewHierarchy() {
-        [label].forEach {
+        [searchBar, tableView].forEach {
             view.addSubview($0)
         }
     }
     
     override func configureViewLayout() {
-        label.snp.makeConstraints {
-            $0.center.equalToSuperview()
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        tableView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom).offset(16)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide)
         }
     }
-
+    
+    override func configureViewDetails() {
+        view.backgroundColor = AppColor.mainBackground.inUIColorFormat
+        tableView.backgroundColor = AppColor.mainBackground.inUIColorFormat
+    }
 }
+
+extension SearchViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.id) as? SearchTableViewCell {
+            cell.fillUpData(with: data[indexPath.row])
+            return cell
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let imageHeight: CGFloat = ( UIScreen.main.bounds.width / 4 ) * 1.2
+        return imageHeight + 16 * 2
+    }
+}
+
+extension SearchViewController : UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {}
+}
+
